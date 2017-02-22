f0aa8c566805c21edbce59e6c2a9f29cfb4551c0
Jon Egeland <jonegeland@gmail.com>
2017-02-22 13:36:21 -0500

    SSH now opens Terminal window in new tab instead of new window Terminal.app doesn't provide an interface to its tab UI, meaning creating a new tab (instead of a new window) requires using osascript and faking a `cmd+t` keypress to make the tab.
    In any case, the work around is more managable than having new windows pop up everytime.

--- lib/wrkflo/steps/ssh.rb
+++ lib/wrkflo/steps/ssh.rb
@@ -5,6 +5,6 @@ class SSHStep < Step
     log "SSHing into  #{config['host']}  at  #{config['directory']}"
 
     ssh_command = "ssh -t #{config['host']} \\\"#{config['directory']}; bash --login\\\""
-    `osascript -e 'tell application "Terminal" to activate' -e 'tell application "Terminal" to set ssh_window to do script "#{ssh_command}"'`
+    `osascript -e 'tell application "Terminal" to activate' -e 'tell application "System Events" to tell process "Terminal" to keystroke "t" using command down' -e 'tell application "Terminal" to do script "#{ssh_command}" in selected tab of the front window'`
   end
 end
