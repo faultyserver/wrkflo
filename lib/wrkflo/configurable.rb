ec6861ce9c493425f703144a7455de1a5773f040
Jon Egeland <jonegeland@gmail.com>
2017-03-02 12:32:08 -0500

    (#4) Add `validate_configuration` to `Step`. `validate_configuration` currently ensures that properties with `required: true` are present. If they are not, the method returns false.

--- lib/wrkflo/configurable.rb
+++ lib/wrkflo/configurable.rb
@@ -18,7 +18,7 @@ module Configurable
   end
 
 
-  def apply_configuration raw_config
+  def apply_configuration raw_config, validate: true
     final_config = self.class.properties.each.with_object({}) do |(name, prop), h|
       provided_value = raw_config[name.to_s]
       # Determine the real value based on the property's definition
@@ -29,6 +29,17 @@ module Configurable
 
     # Create a struct from the configuration hash to enable dot-access.
     @configuration = Struct.new(*final_config.keys).new(*final_config.values)
+    # Always validate the new configuration unless specifically told not to.
+    validate_configuration if validate
+  end
+
+  # Returns a truthy value if the configuration is valid, false otherwise.
+  def validate_configuration
+    self.class.properties.values.each do |prop|
+      if prop.required?
+        return false unless config.respond_to?(prop.name)
+      end
+    end
   end
 
   def config
