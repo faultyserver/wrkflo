093a068bf0a716714db6a2bb2ca358ac0cfda3f7
Jon Egeland <jonegeland@gmail.com>
2017-03-03 11:34:41 -0500

    Move `NFSStep` into `WRKFLO` module, update configurations

--- lib/wrkflo/steps/nfs.rb
+++ lib/wrkflo/steps/nfs.rb
@@ -1,13 +1,19 @@
-class NFSStep < Step
-  add_alias :nfs
+module WRKFLO
+  class NFSStep < Step
+    add_alias :nfs
 
-  def run
-    log "Mounting  #{config['host']}:#{config['remote_path']}  at  #{config['local_path']}"
-    `mount -t nfs #{config['host']}:#{config['remote_path']} #{config['local_path']}`
-  end
+    property :host,         required: true, type: String
+    property :remote_path,  required: true, type: String
+    property :local_path,   required: true, type: String
 
-  def unrun
-    log "Unmounting  #{config['host']}:#{config['remote_path']}  from  #{config['local_path']}"
-    `umount #{config['local_path']}`
+    def run
+      log "Mounting  #{config.host}:#{config.remote_path}  at  #{config.local_path}"
+      `mount -t nfs #{config.host}:#{config.remote_path} #{config.local_path}`
+    end
+
+    def unrun
+      log "Unmounting  #{config.host}:#{config.remote_path}  from  #{config.local_path}"
+      `umount #{config.local_path}`
+    end
   end
 end
